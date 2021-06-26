e8ce2bce52dda966d480d1e382446d70e44e57d7
Sameer Khan <sameerkhan241@outlook.com>
2021-06-26 11:59:20 +0530

    Furnite UI added

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:furniturui/constants.dart';
+import 'package:furniturui/screens/product/products_screen.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Furniture app',
+      theme: ThemeData(
+        // We set Poppins as our default font
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
+        primaryColor: kPrimaryColor,
+        accentColor: kPrimaryColor,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ProductsScreen(),
+    );
+  }
+}
